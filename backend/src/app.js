45d7b25000a288a749a2b4abe999bb858442c32e
Martín Eduardo Torres Platas <martin.torresp@uanl.edu.mx>
2025-07-11 23:53:57 -0600

    Ya definidas las rutas para static y para views. Añadido pug como view engine y creado un index y un navbar base como prueba.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,28 @@
-const express = require('express');
+import express from 'express';
+import path from 'path'
+import {fileURLToPath} from 'url'
 
 const app = express();
 
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const parentDir = path.resolve(__dirname, '..');
+
+//vistas pug
+app.set('view engine', 'pug');
+app.set('views', path.join(parentDir, '../frontend/views'));
+
+//archivos estaticos
+app.use(express.static(path.join(parentDir, '../frontend/public')));
+app.use('/assets', express.static(path.join(parentDir, '../frontend/public')));
+
 const port = 3000;
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}/`);
